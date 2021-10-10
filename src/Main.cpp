f8e27407bbc57e6c81bb38d8c1dddac0d1c48629
Brett Sumser <bsumser@gmail.com>
2021-10-10 00:09:57 -0700

    working on discrete fourier transform

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 #include "../include/Timelord.h"
 
 int main() {
@@ -6,4 +8,20 @@ int main() {
 
     Timelord newTimeLord();
     return 0;
+}
+
+int discreteFourierTransform(std::vector<float> input)
+{
+    int sum = 0;
+
+    //get imaginary number i for DFT algorithm
+    int I = -1;
+    I = sqrt(I);
+
+    //sigma notation algorithm start
+    for (int i = 0; i < input.size() - 1; i++)
+    {
+        input[i] * (exp((-1 * I * 2 * M_PI) / input.size()));
+    }
+    return sum;
 }
