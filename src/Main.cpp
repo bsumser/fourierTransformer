2783dc53fb372cd53297dd4650ee4814bb8b7c7c
Brett Sumser <bsumser@gmail.com>
2021-10-09 22:52:34 -0700

    changes to Timelord execution tracking class

---
+++ src/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../include/Timelord.h"
+
+int main() {
+    std::cout << "Hello world" << std::endl;
+
+    Timelord newTimeLord();
+    return 0;
+}
---
+++ src/Timelord.cpp
@@ -0,0 +1,7 @@
+#include "../include/Timelord.h"
+#include <iostream>
+
+Timelord::Timelord()
+{
+    std::cout << "Class constructor called" << std::endl;
+}
