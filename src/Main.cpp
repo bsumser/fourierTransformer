71ccde9b63ec03e045c102288aebf05bc099ea30
Brett Sumser <bsumser@gmail.com>
2021-11-12 01:47:44 -0800

    no forms of parallel dft working currently

--- src/Main.cpp
+++ src/Main.cpp
@@ -12,6 +12,7 @@ using namespace std;
 vector<float> floatGenerator(int vectorSize);
 vector<std::complex<double>> discreteFourierTransform(vector<complex<double>> input);
 vector<std::complex<double>> discreteFourierTransformFaster(vector<std::complex<double>> input);
+vector<std::complex<double>> discreteFourierTransformTurkey(vector<std::complex<double>> input);
 vector<std::complex<double>> signalGenerator(int sampleSize);
 
 int main(int argc, char* argv[]) {
@@ -21,6 +22,7 @@ int main(int argc, char* argv[]) {
     vector<std::complex<double>> input = signalGenerator(1000);
     discreteFourierTransform(input);
     discreteFourierTransformFaster(input);
+    discreteFourierTransformTurkey(input);
 
     return 0;
 }
@@ -88,16 +90,67 @@ vector<std::complex<double>> discreteFourierTransformFaster(vector<std::complex<
     output.reserve(N);
 
     auto start = high_resolution_clock::now();  //start clock to measure execution time
-    
+
     for (k = 0; k < K; k++)
     {
         innerSum = std::complex<double>(0,0);
         //TODO fix this reduction
-        //#pragma omp parallel for shared(innerSum, input) reduction(+:innerSum)
         for (n = 0; n < N; n++) {
             //process real and imaginary parts of sum via definition in "https://en.wikipedia.org/wiki/Discrete_Fourier_transform"
             double real = cos(((2 * M_PI) / N) * k * n);
             double imag = sin(((2 * M_PI) / N) * k * n);
+
+            //store as std::complex double for multiplication
+            std::complex<double> w (real, -imag);
+            innerSum += input[n] * w;
+        }
+        //add value to the output vector
+        output.push_back(innerSum);
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << __func__ << " returned in " << duration.count() << " microseconds" << endl;
+
+    for (int i = 0; i < 6; i++) {
+        cout << output[i] << endl;
+    }
+    
+    return output;
+}
+
+vector<std::complex<double>> discreteFourierTransformTurkey(vector<std::complex<double>> input)
+{
+    //initiliaze sizes of samples
+    int N = input.size();
+    int K = input.size();
+
+    //init variable for internal loop
+    std::complex<double> innerSum;
+
+    //init vector of std::complex doubles for results
+    vector<std::complex<double>> output;
+
+    //set output vector to have enough space
+    output.reserve(N);
+
+    auto start = high_resolution_clock::now();  //start clock to measure execution time
+    
+    for (int k = 0; k < K; k++)
+    {
+        innerSum = std::complex<double>(0,0);
+        for (int n = 0; n < N / 2 - 1; n++) {
+            //process real and imaginary parts of sum via definition in "https://en.wikipedia.org/wiki/Discrete_Fourier_transform"
+            double real = cos(((2 * M_PI) / (N / 2)) * k * (2 * n));
+            double imag = sin(((2 * M_PI) / (N / 2)) * k * (2 * n));
+
+            //store as std::complex double for multiplication
+            std::complex<double> w (real, -imag);
+            innerSum += input[n] * w;
+        }
+        for (int n = 1; n < N / 2 - 1; n++) {
+            //process real and imaginary parts of sum via definition in "https://en.wikipedia.org/wiki/Discrete_Fourier_transform"
+            double real = cos(((2 * M_PI) / (N / 2)) * k * (2 * n + 1));
+            double imag = sin(((2 * M_PI) / (N / 2)) * k * (2 * n + 1));
 
             //store as std::complex double for multiplication
             std::complex<double> w (real, -imag);
