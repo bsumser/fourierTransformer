9130eab871913754f32ae1da38e19d4911fd041a
Brett Sumser <bsumser@gmail.com>
2021-11-28 13:29:26 -0800

    successully converted image to grayscale

--- src/imageLoader.cpp
+++ src/imageLoader.cpp
@@ -32,5 +32,28 @@ ImageLoader::ImageLoader(const char* path)
             << "a:" << (int)img[i + 3] << " "
             << std::endl;
         }
+
+        std::cout << "attempting to encode grayscale image" << std::endl;
+        for (int i = 0; i < img.size(); i+=4)
+        {
+            char grayPixel;
+            unsigned int gray = ((int)img[i] + (int)img[i + 1] + (int)img[i + 2]) / 3;
+            grayPixel = gray;
+            img[i] = grayPixel;
+            img[i + 1] = grayPixel;
+            img[i + 2] = grayPixel;
+            std::cout << "Pixel " << i << " is : " 
+            << "r:" << (int)img[i] << " " 
+            << "g:" << (int)img[i + 1] << " "
+            << "b:" << (int)img[i + 2] << " "
+            << "a:" << (int)img[i + 3] << " "
+            << std::endl;
+        }
+        char* filename = "test.png";
+
+        unsigned error = lodepng::encode(filename, img, w, h);
+
+        //if there's an error, display it
+        if(error) std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
     }
 }
